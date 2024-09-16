f7065f24a36b7a90eed003eb1749c76b1e2833dc
Hukam Singh Saini <hukamsaini43644@gmail.com>
2024-09-17 00:39:41 +0530

    cookies

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -15,7 +15,7 @@ export const signup = async (req,res,next) => {
     try {
         const {email, password} = req.body;
         if(!email || !password){
-            return res.status(400).send("Email and Password is required");
+            return res.status(400).send("Email and Password is required ");
         }
         const user = await User.create({email, password});
         res.cookie("jwt", createToken(email, user.id),{
